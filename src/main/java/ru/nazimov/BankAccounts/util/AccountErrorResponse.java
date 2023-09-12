ce1a836fa19195b7884a348699399e8679adab43
Ilsur Nazimov <nazimmov@ya.ru>
2023-09-12 16:20:48 +0300

    refactoring

--- src/main/java/ru/nazimov/BankAccounts/util/AccountErrorResponse.java
+++ src/main/java/ru/nazimov/BankAccounts/util/AccountErrorResponse.java
@@ -1,27 +1,16 @@
 package ru.nazimov.BankAccounts.util;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AccountErrorResponse {
     private String message;
     private long timestamp;
 
     public AccountErrorResponse(String message, long timestamp) {
         this.message = message;
-        this.timestamp = timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
 }
